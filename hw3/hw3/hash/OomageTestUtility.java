bb78d0b40d4d4ad2377fd955510605a3e99d929d
indesperate <indesperate@outlook.com>
2022-10-12 21:31:57 +0800

    finish part of hashing

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +13,16 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] array = new int[M];
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            array[bucketNum] += 1;
+        }
+        for (int arr : array) {
+            if (arr > (oomages.size() / 2.5) || arr < (oomages.size() / 50)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
