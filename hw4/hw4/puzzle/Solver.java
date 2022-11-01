b8d2e14f2118cad9b239d1df4667b53e226edd42
indesperate <indesperate@outlook.com>
2022-11-01 15:28:17 +0800

    finish part of hw4

---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,79 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Solver {
+    /* the moves of the nearest path */
+    int moves;
+    /* the nearest path */
+    List<WorldState> path;
+    /* inner search node */
+    private static class SearchNode implements Comparable<SearchNode> {
+        WorldState worldState;
+        int moves;
+        SearchNode previous;
+        SearchNode(WorldState worldState, int moves, SearchNode previous) {
+            this.worldState = worldState;
+            this.moves = moves;
+            this.previous = previous;
+        }
+        @Override
+        public int compareTo(SearchNode o) {
+            return this.moves + this.worldState.estimatedDistanceToGoal() - o.moves - o.worldState.estimatedDistanceToGoal();
+        }
+    }
+    /**
+     * Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution()
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.
+     *
+     * @param initial world initial stat.
+     */
+    public Solver(WorldState initial) {
+        path = new LinkedList<>();
+        MinPQ<SearchNode> minPQ = new MinPQ<>();
+        minPQ.insert(new SearchNode(initial, 0, null));
+        while (true) {
+            SearchNode nearestNode = minPQ.delMin();
+            if (nearestNode.worldState.isGoal()) {
+                this.moves = nearestNode.moves;
+                while (nearestNode.previous.worldState != initial) {
+                    path.add(0, nearestNode.worldState);
+                    nearestNode = nearestNode.previous;
+                }
+                path.add(0, initial);
+                break;
+            }
+            int moves = nearestNode.moves;
+            for (WorldState neighbour : nearestNode.worldState.neighbors()) {
+                minPQ.insert(new SearchNode(neighbour, moves + 1, nearestNode));
+            }
+        }
+    }
+
+    /**
+     * Returns the minimum number of moves to solve the puzzle
+     * starting at the initial WorldState.
+     *
+     * @return the number of moves needed.
+     */
+    public int moves() {
+        return moves;
+    }
+
+    /**
+     * Returns a sequence of WorldStates from the initial WorldState
+     * to the solution
+     *
+     * @return the path to the solution
+     */
+    public Iterable<WorldState> solution() {
+        return path;
+    }
+}
