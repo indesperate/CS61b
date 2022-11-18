7592deb879bcfff49e62b44bc28d9d3b2623854d
indesperate <indesperate@outlook.com>
2022-11-18 22:01:31 +0800

    finish phase one of project 3

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,3 +1,5 @@
+import java.beans.BeanInfo;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,10 +44,59 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
         System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
                            + "your browser.");
+        double lrlon = params.get("lrlon");
+        double ullon = params.get("ullon");
+        double lrlat = params.get("lrlat");
+        double ullat = params.get("ullat");
+        double w = params.get("w");
+        double h = params.get("h");
+        double LonDPPQuery = (lrlon - ullon) / w;
+        double LonDPPOrigin = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / MapServer.TILE_SIZE;
+        int depth = (int)Math.ceil(Math.log(LonDPPOrigin / LonDPPQuery) / Math.log(2));
+        if (depth > 7) {
+            depth = 7;
+        }
+        double latRange = MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT;
+        double lonRange = MapServer.ROOT_LRLON - MapServer.ROOT_ULLON;
+        double latPhase = latRange / Math.pow(2, depth);
+        double lonPhase = lonRange / Math.pow(2, depth);
+        int BeginLatNum = (int)Math.floor((MapServer.ROOT_ULLAT - ullat) / latPhase);
+        int EndLatNum = (int)Math.floor((MapServer.ROOT_ULLAT - lrlat) / latPhase);
+        int BeginLonNum = (int)Math.floor((ullon - MapServer.ROOT_ULLON) / lonPhase);
+        int EndLonNum = (int)Math.floor((lrlon - MapServer.ROOT_ULLON) / lonPhase);
+        if (BeginLonNum < 0) {
+            BeginLonNum = 0;
+        }
+        if (BeginLatNum < 0) {
+            BeginLatNum = 0;
+        }
+        if (EndLonNum >= Math.pow(2,depth)) {
+            EndLonNum = (int)Math.pow(2, depth) - 1;
+        }
+        if (EndLatNum >= Math.pow(2,depth)) {
+            EndLatNum = (int)Math.pow(2, depth) - 1;
+        }
+        int LonNumRange = EndLonNum - BeginLonNum + 1;
+        int LatNumRange = EndLatNum - BeginLatNum + 1;
+        String[][] renderGrid = new String[LatNumRange][LonNumRange];
+        for (int i = 0; i < LatNumRange; i += 1) {
+            for (int j = 0; j < LonNumRange; j += 1) {
+                renderGrid[i][j] = "d" + depth + "_x" + (j + BeginLonNum) + "_y" + (i + BeginLatNum) + ".png";
+            }
+        }
+        System.out.println(Arrays.deepToString(renderGrid));
+        results.put("render_grid", renderGrid);
+        results.put("depth", depth);
+        results.put("raster_ul_lat", MapServer.ROOT_ULLAT - BeginLatNum * latPhase);
+        results.put("raster_lr_lat", MapServer.ROOT_ULLAT - (EndLatNum + 1) * latPhase);
+        results.put("raster_ul_lon", MapServer.ROOT_ULLON + BeginLonNum * lonRange);
+        results.put("raster_lr_lon", MapServer.ROOT_ULLON + (EndLonNum + 1) * lonPhase);
+        results.put("query_success", true);
+        System.out.println("DONE");
         return results;
     }
 
