20dd8566a425066df5ffa15889eb9f0bfbf8296f
indesperate <indesperate@outlook.com>
2022-11-19 14:19:21 +0800

    finish lab12

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.Queue;
 
+import java.util.Random;
+
 public class QuickSort {
     /**
      * Returns a new queue that contains the given queues catenated together.
@@ -47,13 +49,43 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+        for (Item item : unsorted) {
+            int cmp = item.compareTo(pivot);
+            if (cmp == 0) {
+                equal.enqueue(item);
+            } else if (cmp < 0) {
+                less.enqueue(item);
+            }
+            else {
+                greater.enqueue(item);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
+        if (items.size() <= 1) {
+            return items;
+        }
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+        partition(items, getRandomItem(items), less, equal, greater);
+        less = quickSort(less);
+        greater = quickSort(greater);
+        items = catenate(less, equal);
+        items = catenate(items, greater);
         return items;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> randomQueue = new Queue<>();
+        Random random = new Random(10);
+        for (int i = 0; i < 20; i += 1) {
+            randomQueue.enqueue(random.nextInt(1, 100));
+        }
+        System.out.println(randomQueue);
+        System.out.println(quickSort(randomQueue));
     }
 }
