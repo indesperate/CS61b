6bc0cbe842a164181c68720635c7f97ebb969542
indesperate <indesperate@outlook.com>
2022-11-19 14:22:35 +0800

    fix compile error

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -1,17 +1,16 @@
 import edu.princeton.cs.algs4.Queue;
 
-import java.util.Random;
 
 public class MergeSort {
     /**
      * Removes and returns the smallest item that is in q1 or q2.
-     *
+     * <p>
      * The method assumes that both q1 and q2 are in sorted order, with the smallest item first. At
      * most one of q1 or q2 can be empty (but both cannot be empty).
      *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      The smallest item that is in q1 or q2.
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return The smallest item that is in q1 or q2.
      */
     private static <Item extends Comparable> Item getMin(
             Queue<Item> q1, Queue<Item> q2) {
@@ -33,9 +32,11 @@ public class MergeSort {
         }
     }
 
-    /** Returns a queue of queues that each contain one item from items. */
+    /**
+     * Returns a queue of queues that each contain one item from items.
+     */
     private static <Item extends Comparable> Queue<Queue<Item>>
-            makeSingleItemQueues(Queue<Item> items) {
+    makeSingleItemQueues(Queue<Item> items) {
         Queue<Queue<Item>> queues = new Queue<>();
         for (Item item : items) {
             Queue<Item> itemQueue = new Queue<>();
@@ -47,16 +48,15 @@ public class MergeSort {
 
     /**
      * Returns a new queue that contains the items in q1 and q2 in sorted order.
-     *
+     * <p>
      * This method should take time linear in the total number of items in q1 and q2.  After
      * running this method, q1 and q2 will be empty, and all of their items will be in the
      * returned queue.
      *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      A Queue containing all of the q1 and q2 in sorted order, from least to
-     *              greatest.
-     *
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return A Queue containing all of the q1 and q2 in sorted order, from least to
+     * greatest.
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
@@ -68,7 +68,9 @@ public class MergeSort {
         return result;
     }
 
-    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    /**
+     * Returns a Queue that contains the given items sorted from least to greatest.
+     */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
         Queue<Queue<Item>> queues = makeSingleItemQueues(items);
@@ -82,9 +84,8 @@ public class MergeSort {
 
     public static void main(String[] args) {
         Queue<Integer> randomQueue = new Queue<>();
-        Random random = new Random(20);
         for (int i = 0; i < 20; i += 1) {
-            randomQueue.enqueue(random.nextInt(1, 100));
+            randomQueue.enqueue(i * i - 8 * i);
         }
         System.out.println(randomQueue);
         System.out.println(mergeSort(randomQueue));
--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -1,11 +1,10 @@
 import edu.princeton.cs.algs4.Queue;
 
-import java.util.Random;
 
 public class QuickSort {
     /**
      * Returns a new queue that contains the given queues catenated together.
-     *
+     * <p>
      * The items in q2 will be catenated after all of the items in q1.
      */
     private static <Item extends Comparable> Queue<Item> catenate(Queue<Item> q1, Queue<Item> q2) {
@@ -13,13 +12,15 @@ public class QuickSort {
         for (Item item : q1) {
             catenated.enqueue(item);
         }
-        for (Item item: q2) {
+        for (Item item : q2) {
             catenated.enqueue(item);
         }
         return catenated;
     }
 
-    /** Returns a random item from the given queue. */
+    /**
+     * Returns a random item from the given queue.
+     */
     private static <Item extends Comparable> Item getRandomItem(Queue<Item> items) {
         int pivotIndex = (int) (Math.random() * items.size());
         Item pivot = null;
@@ -37,14 +38,14 @@ public class QuickSort {
     /**
      * Partitions the given unsorted queue by pivoting on the given item.
      *
-     * @param unsorted  A Queue of unsorted items
-     * @param pivot     The item to pivot on
-     * @param less      An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are less than the given pivot.
-     * @param equal     An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are equal to the given pivot.
-     * @param greater   An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are greater than the given pivot.
+     * @param unsorted A Queue of unsorted items
+     * @param pivot    The item to pivot on
+     * @param less     An empty Queue. When the function completes, this queue will contain
+     *                 all of the items in unsorted that are less than the given pivot.
+     * @param equal    An empty Queue. When the function completes, this queue will contain
+     *                 all of the items in unsorted that are equal to the given pivot.
+     * @param greater  An empty Queue. When the function completes, this queue will contain
+     *                 all of the items in unsorted that are greater than the given pivot.
      */
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
@@ -55,14 +56,15 @@ public class QuickSort {
                 equal.enqueue(item);
             } else if (cmp < 0) {
                 less.enqueue(item);
-            }
-            else {
+            } else {
                 greater.enqueue(item);
             }
         }
     }
 
-    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    /**
+     * Returns a Queue that contains the given items sorted from least to greatest.
+     */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
         if (items.size() <= 1) {
@@ -81,9 +83,8 @@ public class QuickSort {
 
     public static void main(String[] args) {
         Queue<Integer> randomQueue = new Queue<>();
-        Random random = new Random(10);
         for (int i = 0; i < 20; i += 1) {
-            randomQueue.enqueue(random.nextInt(1, 100));
+            randomQueue.enqueue(i * i - 8 * i);
         }
         System.out.println(randomQueue);
         System.out.println(quickSort(randomQueue));
